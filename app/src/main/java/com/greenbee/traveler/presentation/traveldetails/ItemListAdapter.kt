aa8793845d7a0e6a6051430fda6c6b55c18310cd
mjwa <mikolaj.wozniak@gft.com>
2020-05-27 13:16:01 +0200

    Prepare place on categories and items lists for add new functionality

--- app/src/main/java/com/greenbee/traveler/presentation/traveldetails/ItemListAdapter.kt
+++ app/src/main/java/com/greenbee/traveler/presentation/traveldetails/ItemListAdapter.kt
@@ -4,33 +4,82 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.greenbee.traveler.R
 import com.greenbee.traveler.domain.entities.Item
-import kotlinx.android.synthetic.main.item_item.view.*
+import kotlinx.android.synthetic.main.item_list_add_item.view.*
+import kotlinx.android.synthetic.main.item_list_item.view.*
+
+private val ADD_ITEM_TYPE = 1
+private val ITEM_TYPE = 0
 
 class ItemListAdapter(var clickListener: ItemListener) :
-    ListAdapter<Item, ItemListAdapter.ItemViewHolder>(ItemDiffUtilCallback()) {
+    ListAdapter<Item, RecyclerView.ViewHolder>(ItemDiffUtilCallback()) {
 
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        return ItemViewHolder(
-            inflater.inflate(
-                R.layout.item_item,
-                parent,
-                false
+
+        return when (viewType) {
+            ADD_ITEM_TYPE -> AddItemViewHolder(
+                inflater.inflate(
+                    R.layout.item_list_add_item,
+                    parent,
+                    false
+                )
             )
-        )
+            else -> ItemViewHolder(
+                inflater.inflate(
+                    R.layout.item_list_item,
+                    parent,
+                    false
+                )
+            )
+        }
+
     }
 
-    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
-        getItem(position).let {
-            holder.name.text = it.name
-            holder.checkBox.isChecked = it.isDone
+    override fun getItemCount(): Int {
+        return super.getItemCount() + 1
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return if (position == itemCount - 1) ADD_ITEM_TYPE else ITEM_TYPE
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (position == itemCount - 1) {
+            (holder as AddItemViewHolder)
+            holder.newButton.setOnClickListener {
+                holder.newButton.visibility = View.GONE
+                holder.name.visibility = View.VISIBLE
+                holder.confirm.visibility = View.VISIBLE
+                holder.cancel.visibility = View.VISIBLE
+            }
+            holder.confirm.setOnClickListener {
+                holder.newButton.visibility = View.VISIBLE
+                holder.name.visibility = View.GONE
+                holder.confirm.visibility = View.GONE
+                holder.cancel.visibility = View.GONE
+            }
+            holder.cancel.setOnClickListener {
+                holder.newButton.visibility = View.VISIBLE
+                holder.name.visibility = View.GONE
+                holder.confirm.visibility = View.GONE
+                holder.cancel.visibility = View.GONE
+            }
+
+
+        } else {
+            getItem(position).let {
+                (holder as ItemViewHolder).name.text = it.name
+                holder.checkBox.isChecked = it.isDone
+            }
         }
     }
 
@@ -40,6 +89,13 @@ class ItemListAdapter(var clickListener: ItemListener) :
             oldItem.id == newItem.id
 
         override fun areContentsTheSame(oldItem: Item, newItem: Item): Boolean = oldItem == newItem
+    }
+
+    class AddItemViewHolder(root: View) : RecyclerView.ViewHolder(root) {
+        val newButton: ImageButton = root.imageButton
+        val name: EditText = root.new_item_name
+        val confirm: ImageButton = root.confirm
+        val cancel: ImageButton = root.cancel
     }
 
     class ItemViewHolder(root: View) : RecyclerView.ViewHolder(root) {
